7e0da62ca5885393ec1f16fa7f212dd79dfdd3bf
Hideichi Kubo <unthought_known@outlook.jp>
2024-12-01 10:21:12 +0900

    ユニット5_パスウェイ2_練習: Amphibians アプリを作成する AmphibiansApp作成完了

---
+++ Amphibians/app/src/main/java/com/example/amphibians/ui/screens/HomeScreen.kt
@@ -0,0 +1,184 @@
+package com.example.amphibians.ui.screens
+
+import android.content.Context
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.amphibians.R
+import com.example.amphibians.model.Amphibian
+import com.example.amphibians.ui.theme.AmphibiansTheme
+
+@Composable
+fun HomeScreen(
+    amphibiansUiState: AmphibiansUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp)
+) {
+    when (amphibiansUiState) {
+        is AmphibiansUiState.Loading -> LoadingScreen(modifier.size(200.dp))
+        is AmphibiansUiState.Success ->
+            AmphibiansListScreen(
+                amphibians = amphibiansUiState.amphibians,
+                modifier = modifier
+                    .padding(
+                        start = dimensionResource(R.dimen.padding_medium),
+                        top = dimensionResource(R.dimen.padding_medium),
+                        end = dimensionResource(R.dimen.padding_medium)
+                    ),
+            )
+        else ->
+            ErrorScreen(
+                retryAction = retryAction,
+                modifier = modifier
+            )
+    }
+}
+
+@Composable
+fun AmphibianCard(
+    amphibian: Amphibian,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(dimensionResource(R.dimen.padding_small))
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "${amphibian.name}(${amphibian.type})",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(dimensionResource(R.dimen.padding_medium)),
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Start
+            )
+            AsyncImage(
+                model = ImageRequest.Builder(context = LocalContext.current)
+                    .data(amphibian.imgSrc)
+                    .crossfade(true)
+                    .build(),
+                error = painterResource(R.drawable.broken_image),
+                placeholder = painterResource(R.drawable.cached),
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Text(
+                text = amphibian.description,
+                textAlign = TextAlign.Justify,
+                modifier = Modifier
+                    .padding(dimensionResource(R.dimen.padding_medium))
+            )
+        }
+    }
+}
+
+@Composable
+fun AmphibiansListScreen(
+    amphibians: List<Amphibian>,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp)
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = contentPadding,
+        verticalArrangement = Arrangement.spacedBy(24.dp)
+    ) {
+        items(
+            items = amphibians,
+            key = { amphibian ->
+                amphibian.name
+            }
+        ) { amphibian ->
+            AmphibianCard(
+                amphibian = amphibian,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+    }
+}
+
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(R.drawable.cached),
+        contentDescription = stringResource(R.string.loading),
+        modifier = modifier
+    )
+}
+
+@Composable
+fun ErrorScreen(retryAction: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(stringResource(R.string.loading_failed))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun AmphibianCardPreview() {
+//    AmphibiansTheme {
+//        AmphibianCard(
+//            amphibian = Amphibian(
+//                name = "Great Basin Spadefoot",
+//                type = "Toad",
+//                description = "This toad spends most of its life underground due to the arid desert conditions in which it lives. Spadefoot toads earn the name because of their hind legs which are wedged to aid in digging. They are typically grey, green, or brown with dark spots.",
+//                imgSrc = "https://developer.android.com/codelabs/basic-android-kotlin-compose-amphibians-app/img/great-basin-spadefoot.png"
+//            )
+//        )
+//    }
+//}
+
+@Preview(showBackground = true)
+@Composable
+fun AmphibiansListScreenPreview() {
+    AmphibiansTheme {
+        val mockData = List(10) {
+            Amphibian(
+                "Lorem Ipsum - $it",
+                "$it",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do" +
+                        " eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad" +
+                        " minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip" +
+                        " ex ea commodo consequat.",
+                imgSrc = ""
+            )
+        }
+        AmphibiansListScreen(mockData, Modifier.fillMaxSize())
+    }
+}
