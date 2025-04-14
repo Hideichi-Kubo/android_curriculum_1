76855047c8fc974f4109991b94f9b9ccedbd760f
Hideichi Kubo <unthought_known@outlook.jp>
2025-04-14 16:48:45 +0900

    JUnit Sample

--- app/src/main/java/com/example/junitsample/util/AppUtils.kt
+++ app/src/main/java/com/example/junitsample/util/AppUtils.kt
@@ -1,2 +1,5 @@
 package com.example.junitsample.util
 
+fun isAdult(age: Int): Boolean {
+    return if (age >= 20) true else false
+}
--- app/src/test/java/com/example/junitsample/AppUtilsTest.kt
+++ app/src/test/java/com/example/junitsample/AppUtilsTest.kt
@@ -1,4 +1,23 @@
 package com.example.junitsample
 
+import com.example.junitsample.util.isAdult
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
 class AppUtilsTest {
+
+    @Test
+    fun testIsAdult_WhenAgeIs20OrMore_ShouldReturnTrue() {
+        // 境界値およびそれ以上の値が大人と判定されるかテストする
+        assertTrue("20歳は大人と判定されるはず", isAdult(25))
+        assertTrue("30歳は大人と判定されるはず", isAdult(35))
+    }
+
+    @Test
+    fun testIsAdult_WhenAgeIsLessThan20_ShouldReturnFalse() {
+        // 20未満の場合に大人とならないことを検証する
+        assertFalse("19歳は大人と判定されないはず", isAdult(19))
+        assertFalse("10歳は大人と判定されないはず", isAdult(-1))
+    }
 }
